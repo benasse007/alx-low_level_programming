dcf920dacd0ae918ead5ba11beea63ceb2cc8e6e
ben <ferdinandkoli09@gmail.com>
2022-07-04 09:23:11 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - prints "Programming  is like  building a multilingual puzzle"
- *
- *   * @void: makes no parameter intake
- *
- *    *
- *
- *     * Return: this program doesn't seem to return anything  */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-	    puts("\"Programming is like building a multilingual puzzle");
-
-	        return (0);
-
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
