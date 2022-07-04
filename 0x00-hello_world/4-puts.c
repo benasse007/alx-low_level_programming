631f7d27f8ea76c63f3614ec8b5c490d6ddfce5a
benasse007 <ferdinandkoli09@gmail.com>
2022-07-03 17:13:55 -0700

    reponse

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *
+ *  * main - prints "Programming  is like  building a multilingual puzzle"
+ *
+ *   * @void: makes no parameter intake
+ *
+ *    *
+ *
+ *     * Return: this program doesn't seem to return anything
+ *
+ *     */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
