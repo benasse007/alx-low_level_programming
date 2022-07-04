07205d682ac395d17a67a543955ed8cf1c8f4253
ben <ferdinandkoli09@gmail.com>
2022-07-04 12:03:23 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+/**
+ * main - prints hexadecimal letter
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
